f8772aad37468d94971f3742e7de97954383f802
Huong Dang <miley.dawn@codeimmersives.com>
2020-12-06 12:36:13 -0500

    install jsonwebtoken and encode user information after creating user

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter);
 // app.use('/recipes', recipeRouter);
 
 // catch 404 and forward to error handler
--- routes/user/userRoute.js
+++ routes/user/userRoute.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('./User')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 
 /* GET users listing. */
@@ -18,11 +19,29 @@ router.post('/create-user', async (req, res, next) => {
       email: req.body.email,
       password: hashedPassword,
     })
+    
     newUser.save()
-    res.status(200).json({ message: 'User created', user: newUser });
+    console.log('newUser', newUser)
+    console.log('line24 ====')
+    
+    const jwtToken = await jwt.sign({email: newUser.email, _id: newUser._id}, 'thisIsASecretPassWord', {expiresIn: '1h'})
+    // const token = jwt.sign({email: foundEmail.email, _id: foundEmail._id}, "thisIsASecretCode", {expiresIn: '1h'})
+    console.log('line27 ====')
+    console.log('jwtToken', jwtToken)
+
+    // res.status(200).send({ 
+    //   message: 'Do you fucking see this text?'});
+
+    res.status(200).json({ 
+      message: 'User created', 
+      email: newUser.email, 
+      password: newUser.password, 
+      _id: newUser._id, 
+      jwtToken: jwtToken});
   }
   catch (e) {
-    res.state(500).json({ message: e })
+    console.log('e', e)
+    res.status(500).json({ message: e })
   }
 });
 
